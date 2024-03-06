8104c5a14a6e740cd113ea82ee14eeb948da6a7b
miguel17266 <mbozop@gmail.com>
2024-03-05 22:12:52 -0300

    Act Prentrega2 JS

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+const hamburguesas = [
+    {id: 1, nombre: "italiana",precio: 5000 },
+    {id: 2, nombre: "completa", precio: 6000 },
+    {id: 3, nombre: "vegana", precio: 5500 },
+    {id: 4, nombre: "chacarera",precio: 7000 },
+]
 
 
 function saludar(nombre) {
@@ -11,19 +17,81 @@ saludar(nombreIngresado)
 if (nombreIngresado == "") {
     alert("Campo vacio");
 }
+} while (nombreIngresado == "");
+
+    HamburguesaElegida = prompt("Bienvenido " + nombreIngresado + " Por favor elige tu hamburguesa para continuar. \n Italiana: $5.000 \n Completa: $6.000 \n Vegana: $5.500 \n Chacarera: $7.000")
+    let HamburguesaEncontrada= hamburguesas.find(hamburguesa => hamburguesa.nombre === HamburguesaElegida);
+    if (HamburguesaEncontrada) {
+    alert("Has elegido " + hamburguesaEncontrada.nombre + ". Precio: $" + hamburguesaEncontrada.precio);
+} else {
+    alert("Hamburguesa no encontrada");
+}
+
+/* let nombreIngresado
+let eligeHamburguesa
+
+function saludar(nombre) {
+    console.log("Hola " + nombre);
+    return ("Hola "+ nombre);
+}
+
+
+do {
+nombreIngresado = prompt("¡Hola! Bienvenido a McJavaS. Por favor ingresa tu nombre para ser atendido")
+saludar(nombreIngresado)
+if (nombreIngresado == "") {
+    alert("Campo vacio");
+}
 } while (nombreIngresado == "") {
 }
+do {
     eligeHamburguesa = prompt("Bienvenido " + nombreIngresado + " Por favor elige tu hamburguesa para continuar. \n Hamburguesa 1: $2.000 \n Hamburguesa 2: $2.500 \n Hamburguesa 3: $3.000")
     console.log("ELegiste " + eligeHamburguesa);
     if (eligeHamburguesa == "") {
         alert("Campo vacio");
-    } else { 
+    }}
+    while (eligeHamburguesa == " ") {
+    
+    } { 
             if (eligeHamburguesa == "Hamburguesa 1" || eligeHamburguesa == "Hamburguesa 2" ||eligeHamburguesa == "Hamburguesa 3")
             eligeBebida = prompt("Elegiste " + eligeHamburguesa + " ¿Quieres agregar papas fritas a tu pedido? \n Papas pequeñas: $1.000 \n Papas medianas: $1.500 \n Papas familiares: $2.000")
         console.log("ELegiste " + eligeHamburguesa);
-        }
-    
+        } */
 
+//Variables
+/* let nombre= prompt("Ingrese su nombre");
+console.log("Hola " + nombre);
+
+let numeroA = Number(prompt("Ingresa un número"))
+let numeroB = Number(prompt("Ingresa otro número"))
+let resultado = numeroA + numeroB
+if (resultado >20) {
+alert ("El número " + resultado + "es menor a 20");  
+} else if (resultado >40) {
+    alert ("El número " + resultado + "es menor a 40"); {
+    }else{
+        alert ("El número " + resultado + "es menor a 60"); 
+    }
+} */
+
+/* let nombre = prompt("Nombre")
+while (nombre == " ") {
+    alert ("nombre no válido")
+    
+} */
+
+/* class Producto {
+  constructor(nombre, apellido, anio) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.anio = anio;
+    this.saludar = function () {
+      console.log("Hola! Soy " + this.nombre);
+    };
+  }
+}
+const producto1 = new Producto("Miguel", "Bozo", 34);
+producto1.saludar(); */
 /* let nombre = "Canelita"
 let apellido = "Bozo"
 let edad = 27
@@ -36,7 +104,6 @@ console.log(anioActual * anioNacimiento);
 
 let nombreUsuario = prompt("Ingrese su nombre")
 console.log(nombreUsuario); */
-
 
 /* ESTRUCTURA BASICA IF/ELSE
 if (condición que se tiene que cumplir) {
@@ -86,9 +153,7 @@ alert("Has escogido " + " " + eligeHamburguesa);
     alert("Por favor escoge una haburguesa")
 } */
 
-
 /* ! Operador Not */
-
 
 //For
 /* for (let x = 0; x <=10; x++){
@@ -125,13 +190,12 @@ switch (adivinaNumero) {
         break;
 } */
 
-
 //FUNCIONES: estructuras que nos permiten ejecutar una o varias sentencias varias veces dentro del código. No se ejecutan hasta que no son llamadas. Admiten parámetros, que son variables dentro de la función, van entre paréntesis y sirven para que al invocar la función, podamos indicarle el valor del parámetro
 //Funciones Declaradas
 /* function Saludar(nombre) {//Palabra reservada Nombre (Parámetros)
     console.log("Hola " + nombre);
 }
-Saludar("Miguel") *///Llamado a la función
+Saludar("Miguel") */ //Llamado a la función
 
 //Funciones Anónimas, guardan el valor dentro de una constante.
 /* const Saludar = function (params) {
@@ -148,3 +212,10 @@ Saludar(); */
 return a + b
 }
 console.log(suma(3, 5)); */
+
+/* const zelda = videoJuegos.filter((videojuego) => videojuego === "zelda")
+console.log(zelda); */
+
+/* const nombre constante = array.metodo ((nombre elemento) => condición que queremos que se cumpla) */
+
+console.dir(document.head)
